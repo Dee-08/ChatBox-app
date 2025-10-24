abb5822d4031236505a9f3c25160b9421a5926e1
Daniel <danielibukun2000@gmail.com>
2025-10-24 12:00:12 +0100

    User Model updated

--- lib/core/constant/app_client.dart
+++ lib/core/constant/app_client.dart
@@ -2,7 +2,7 @@ import 'package:flint_client/flint_client.dart';
 
 class AppClient {
   static final client = FlintClient(
-    baseUrl: "http://10.0.0.0:3000",
+    baseUrl: "http://10.0.2.2:3000",
     debug: true,
     onError: (error) {
       print(error.message);
--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -10,6 +10,7 @@ class UserModel {
   final DateTime? emailVerifyAt;
   final bool? isVerified;
   final DateTime? createdAt;
+  final DateTime? updatedAt;
 
   UserModel({
     required this.id,
@@ -17,6 +18,7 @@ class UserModel {
     required this.name,
     this.profilePicUrl,
     this.createdAt,
+    this.updatedAt,
     this.emailVerifyAt,
     this.isVerified,
     this.provider,
@@ -31,9 +33,37 @@ class UserModel {
       'profilePicUrl': profilePicUrl,
       'provider': provider,
       'providerId': providerId,
-      'emailVerifyAt': emailVerifyAt?.millisecondsSinceEpoch,
+      'emailVerifyAt': emailVerifyAt,
       'isVerified': isVerified,
-      'createdAt': createdAt?.millisecondsSinceEpoch,
+      'createdAt': createdAt,
+      'updatedAt': createdAt,
     };
   }
+
+  static UserModel? fromMap(Map<String, dynamic>? map) {
+    if (map == null) return null;
+    try {
+      return UserModel(
+        id: map['id'],
+        name: map['name'],
+        email: map['email'],
+        profilePicUrl: map['profilePicUrl'],
+        provider: map['provider'],
+        providerId: map['providerId'],
+        emailVerifyAt: DateTime.fromMillisecondsSinceEpoch(
+          map['emailVerifyAt'],
+        ),
+        isVerified: map['isVerified'],
+        createdAt: DateTime.tryParse(map['createdAt']),
+        updatedAt: DateTime.tryParse(map['updatedAt']),
+      );
+    } catch (e) {
+      return null;
+    }
+  }
+
+  String toJson() => json.encode(toMap());
+
+  static UserModel? fromJson(String source) =>
+      UserModel.fromMap(json.decode(source));
 }
